b7cbe1469f01563e00c6c72bf4d1c73f46c60502
Ramin <raminramazanpour@gmail.com>
2025-12-06 02:21:52 +0330

    add nextConfig type

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
+import type { NextConfig } from 'next'
+
 const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   typedRoutes: true,
