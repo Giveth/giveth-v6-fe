9635deca9c5edfcb2557777519ec5db7fadc505a
Ramin <raminramazanpour@gmail.com>
2025-12-07 14:53:16 +0330

    use generated graphql

--- src/hooks/useQfRoundStats.ts
+++ src/hooks/useQfRoundStats.ts
@@ -1,30 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import { graphQLClient } from '@/lib/graphql/client'
-import { graphql } from '@/lib/graphql/generated'
-
-const qfRoundStatsQuery = graphql(`
-  query QfRoundStats($qfRoundId: Int!) {
-    qfRoundStats(qfRoundId: $qfRoundId) {
-      totalDonationsUsd
-      donationsCount
-      uniqueDonors
-      qfRound {
-        id
-        allocatedFundUSD
-        allocatedTokenSymbol
-        beginDate
-        endDate
-      }
-    }
-  }
-`)
+import { QfRoundStatsDocument } from '@/lib/graphql/generated/graphql'
 
 export const useQfRoundStats = (qfRoundId?: number) => {
   return useQuery({
     queryKey: ['qfRoundStats', qfRoundId],
     queryFn: async () => {
       if (!qfRoundId) return null
-      return graphQLClient.request(qfRoundStatsQuery, { qfRoundId })
+      return graphQLClient.request(QfRoundStatsDocument, { qfRoundId })
     },
     enabled: !!qfRoundId,
   })
