21c4e8c766290614681a9c5e3269b64a2d7d4639
kkatusic <kkatusic@gmail.com>
2026-02-02 14:13:36 +0100

    removed unwanted change

--- src/hooks/useUpcomingQfRounds.ts
+++ src/hooks/useUpcomingQfRounds.ts
@@ -1,6 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import { graphQLClient } from '@/lib/graphql/client'
-import { type QfRoundsQuery } from '@/lib/graphql/generated/graphql'
+import {
+  QfRoundStatusFilter,
+  type QfRoundsQuery,
+} from '@/lib/graphql/generated/graphql'
 import { qfRoundsQuery } from '@/lib/graphql/queries'
 
 /**
@@ -10,23 +13,11 @@ export const useUpcomingQfRounds = () => {
   return useQuery<QfRoundsQuery>({
     queryKey: ['upcomingQfRounds'],
     queryFn: async () => {
-      const data = await graphQLClient.request(qfRoundsQuery, {
+      return graphQLClient.request(qfRoundsQuery, {
         skip: 0,
         take: 50,
+        filters: { status: QfRoundStatusFilter.Upcoming },
       })
-      const now = Date.now()
-      return {
-        ...data,
-        qfRounds: {
-          ...data.qfRounds,
-          rounds: data.qfRounds.rounds.filter(round => {
-            const begin = round.beginDate
-              ? new Date(round.beginDate).getTime()
-              : 0
-            return begin > now
-          }),
-        },
-      }
     },
     staleTime: 1000 * 60 * 5,
   })
