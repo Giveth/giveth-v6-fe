4ffad47de0f1840190eba5e7d78f7f1ce4a6f90e
kkatusic <kkatusic@gmail.com>
2026-02-11 18:02:09 +0100

    starting config to fetch data from the chain about rewards

--- src/lib/constants/staking-power-constants.tsx
+++ src/lib/constants/staking-power-constants.tsx
@@ -36,3 +36,23 @@ export const PROD_STAKING_CHAINS = [
 // For all chains we use these constants
 export const STAKING_CHAINS =
   env.VERCEL_ENV === 'production' ? PROD_STAKING_CHAINS : DEV_STAKING_CHAINS
+
+export const DEV_STAKING_POOLS = [
+  {
+    OPTIMISM_CONFIG: {
+      GIVPOWER: {
+        network: 11155420, // Optimism Sepolia chain ID
+        LM_ADDRESS: '0xE6836325B13819CF38f030108255A5213491A725',
+        POOL_ADDRESS: '0x2f2c819210191750F2E11F7CfC5664a0eB4fd5e6', // GIV token on OP Sepolia
+        type: 'GIV_UNIPOOL_LM',
+        platform: 'Staking',
+        title: 'GIV',
+        description: '100% GIV',
+        unit: 'GIV',
+      },
+    },
+  },
+]
+
+export const STAKING_POOLS =
+  env.VERCEL_ENV === 'production' ? DEV_STAKING_POOLS : DEV_STAKING_POOLS
